a05a0fb414e3ec0dac82ca13cb326cfa8502826c
Ben Siraphob <bensiraphob@gmail.com>
2023-07-19 23:49:06 +0700

    WIP2

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE DataKinds #-}
 module Main where
 
 import GHC.Natural
 import Control.Monad
 import System.Environment (getEnv)
 
-import qualified Paths_hevm as Paths
+-- import qualified Paths_hevm as Paths
 
 import Test.Tasty (localOption, withResource)
 import Test.Tasty.Bench
@@ -32,15 +33,27 @@ import qualified EVM.FeeSchedule as FeeSchedule
 import EVM (initialContract, makeVm)
 import qualified Data.ByteString as ByteString
 import Bytecodes
+import EVM.Test.Tracing
+import Data.Aeson qualified as JSON
+import System.Process (readProcessWithExitCode)
+import GHC.IO.Exception
+import System.Exit (ExitCode(..), exitWith, ExitCode(..))
+import GHC.Word
+import Data.Maybe
+import EVM.Sign
 
+-- import EVM.Types
+
+-- benchmark hevm and EVM tool on the folder called benchmarks
+-- using tasty-bench
 main :: IO ()
-main = defaultMain
-  [ mkbench erc20 "erc20" 0 [1]
-  , mkbench (pure vat) "vat" 0 [4]
-  , mkbench (pure deposit) "deposit" 32 [4]
-  , mkbench (pure uniV2Pair) "uniV2" 10 [4]
-  , withResource bcjsons (pure . const ()) blockchainTests
-  ]
+main = do
+  let path = "benchmarks"
+  hevmBenches <- benchFolder path
+  evmtoolBenches <- benchEVMToolFolder path
+  defaultMain [ bgroup "hevm" hevmBenches
+              , bgroup "evmtool" evmtoolBenches
+              ]
 
 vmFromByteString :: ByteString -> VM
 vmFromByteString bs = EVM.Transaction.initTx vm0
@@ -100,6 +113,95 @@ benchFolder path = do
   files <- Find.find Find.always (Find.extension Find.==? ".bin") path
   pure $ map benchFile files
 
+-- bytecodeToOpContract :: ByteString -> OpContract
+-- evmSetup :: OpContract -> ByteString -> Int -> (EVM.Transaction.Transaction, EVMToolEnv, EVMToolAlloc, Addr, Addr, Integer)
+evmSetup' contr txData gaslimitExec = (txn, evmEnv, contrAlloc, fromAddress, toAddress, sk)
+  where
+    contrLits = contr
+    -- toW8fromLitB :: Expr 'Byte -> Word8
+    -- toW8fromLitB (LitByte a) = a
+    -- toW8fromLitB _ = internalError "Cannot convert non-litB"
+
+    bitcode = ByteString.pack $ contrLits
+    contrAlloc = EVMToolAlloc{ balance = 0xffffffffffffff
+                             , code = bitcode
+                             , nonce = 0x48
+                             }
+    txn = EVM.Transaction.Transaction
+      { txdata     = txData
+      , gasLimit = fromIntegral gaslimitExec
+      , gasPrice = Just 1
+      , nonce    = 172
+      , toAddr   = Just 0x8A8eAFb1cf62BfBeb1741769DAE1a9dd47996192
+      , r        = 0 -- will be fixed when we sign
+      , s        = 0 -- will be fixed when we sign
+      , v        = 0 -- will be fixed when we sign
+      , value    = 0 -- setting this > 0 fails because HEVM doesn't handle value sent in toplevel transaction
+      , txtype     = EVM.Transaction.EIP1559Transaction
+      , accessList = []
+      , maxPriorityFeeGas =  Just 1
+      , maxFeePerGas = Just 1
+      , chainId = 1
+      }
+    evmEnv = EVMToolEnv { coinbase   =  0xff
+                        , timestamp   =  Lit 0x3e8
+                        , number      =  0x0
+                        , prevRandao  =  0x0
+                        , gasLimit    =  fromIntegral gaslimitExec
+                        , baseFee     =  0x0
+                        , maxCodeSize =  0xfffff
+                        , schedule    =  FeeSchedule.berlin
+                        , blockHashes =  blockHashesDefault
+                        }
+    sk = 0xDC38EE117CAE37750EB1ECC5CFD3DE8E85963B481B93E732C5D0CB66EE6B0C9D
+    fromAddress :: Addr
+    fromAddress = fromJust $ deriveAddr sk
+    toAddress :: Addr
+    toAddress = 0x8A8eAFb1cf62BfBeb1741769DAE1a9dd47996192
+
+
+
+-- getEVMToolRet' :: OpContract -> ByteString -> Int -> IO (Maybe EVMToolResult)
+runGeth contr = do
+  let (txn, evmEnv, contrAlloc, fromAddress, toAddress, sk) = evmSetup' contr mempty 40000
+      txs = [EVM.Transaction.sign sk txn]
+      walletAlloc = EVMToolAlloc{ balance = 0x5ffd4878be161d74
+                                , code = mempty -- ByteString.pack [ 0x60, 0x20 ]
+                                , nonce = 0xac
+                                }
+      alloc :: Map.Map Addr EVMToolAlloc
+      alloc = Map.fromList ([ (fromAddress, walletAlloc), (toAddress, contrAlloc)])
+  JSON.encodeFile "txs.json" txs
+  JSON.encodeFile "alloc.json" alloc
+  JSON.encodeFile "env.json" evmEnv
+  (exitCode, evmtoolStdout, evmtoolStderr) <- readProcessWithExitCode "evm" [ "transition"
+                               ,"--input.alloc" , "alloc.json"
+                               , "--input.env" , "env.json"
+                               , "--input.txs" , "txs.json"
+                               , "--output.alloc" , "alloc-out.json"
+                               -- , "--trace.returndata=true"
+                               , "--trace" , "trace.json"
+                               , "--output.result", "result.json"
+                               ] ""
+  when (exitCode /= ExitSuccess) $ do
+    putStrLn $ "evmtool exited with code " <> show exitCode
+    putStrLn $ "evmtool stderr output:" <> show evmtoolStderr
+    putStrLn $ "evmtool stdout output:" <> show evmtoolStdout
+    exitWith (ExitFailure 2)
+    
+
+runGethOnFile path = do
+  bs <- strip0x <$> ByteString.readFile path
+  let contr = ByteString.unpack bs
+  runGeth contr
+benchEVMTool :: FilePath -> Benchmark
+benchEVMTool path = bench (show path) $ nfIO $ runGethOnFile path
+
+benchEVMToolFolder :: FilePath -> IO [Benchmark]
+benchEVMToolFolder path = do
+  files <- Find.find Find.always (Find.extension Find.==? ".bin") path
+  pure $ map benchEVMTool files
+
 --- General State Tests ----------------------------------------------------------------------------
 
 
@@ -117,7 +219,7 @@ bcjsons = do
     parseSuite path = do
       contents <- LazyByteString.readFile path
       case BCTests.parseBCSuite contents of
-        Left e -> pure (path, mempty)
+        Left _ -> pure (path, mempty)
         Right tests -> pure (path, tests)
 
 -- | executes all provided bc tests in sequence and accumulates a boolean value representing their success.
@@ -160,7 +262,7 @@ debugContract c = withSolvers CVC5 4 Nothing $ \solvers -> do
 
 findPanics :: Solver -> Natural -> Integer -> ByteString -> IO ()
 findPanics solver count iters c = do
-  (_, res) <- withSolvers solver count Nothing $ \s -> do
+  (_, _) <- withSolvers solver count Nothing $ \s -> do
     let opts = defaultVeriOpts
           { maxIter = Just iters
           , askSmtIters = iters + 1
